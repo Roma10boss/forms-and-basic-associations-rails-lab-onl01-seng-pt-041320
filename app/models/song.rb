2d89a4ae77eda2a16a9b9b23aa016ebe98b2aeb8
Romario Gustave <romagustave10@gmail.com>
2020-09-16 09:39:45 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,29 +1,52 @@
-class Song < ActiveRecord::Base
-  belongs_to :artist
-    belongs_to :genre
-    has_many :notes
-
-
-    def artist_name=(name)
-      self.artist = Artist.find_or_create_by(name: name)
-
-    end
-
-    def artist_name
-      self.artist ? self.artist.name : nil
-
-    end
-
-    def note_contents
-      notes.collect {|note| note.content}
-
-    end
-
-    def note_contents=(notes)
-      notes.each do |content|
-        note = self.notes.build(content: content) unless content.empty?
-
-      end
-    end
-
+class SongsController < ApplicationController
+  def index
+    @songs = Song.all
   end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params)
+
+    if @song.save
+      redirect_to @song
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+
+    @song.update(song_params)
+
+    if @song.save
+      redirect_to @song
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @song = Song.find(params[:id])
+    @song.destroy
+    flash[:notice] = "Song deleted."
+    redirect_to songs_path
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:title, :artist_name, :genre_id, note_contents: [])
+  end
+end
